040734bb084d1ba5530cd013715d22e706469345
appreciated <goebeljohannes@gmx.net>
2017-07-20 11:56:47 +0200

    - updated test UI

--- HybridMenu-demo/src/main/java/kaesdingeling/hybridmenu/demo/DemoUI.java
+++ HybridMenu-demo/src/main/java/kaesdingeling/hybridmenu/demo/DemoUI.java
@@ -7,13 +7,13 @@ import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.server.VaadinSession;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
+import kaesdingeling.hybridmenu.data.CustomMenuItem;
 import kaesdingeling.hybridmenu.data.MenuItemBuilder;
 import kaesdingeling.hybridmenu.demo.page.HomePage;
 import kaesdingeling.hybridmenu.demo.page.MemberPage;
-import kaesdingeling.hybridmenu.enums.EMenuItemPosition;
-import kaesdingeling.hybridmenu.enums.EMenuType;
-import kaesdingeling.hybridmenu.enums.ETopMenuPosition;
+import kaesdingeling.hybridmenu.enums.*;
 import kaesdingeling.hybridmenu.menu.HybridMenu;
 import kaesdingeling.hybridmenu.menu.HybridMenuBuilder;
 import kaesdingeling.hybridmenu.page.DefaultPage;
@@ -34,29 +34,31 @@ public class DemoUI extends UI {
         HybridMenu hybridMenu = HybridMenuBuilder.get()
                 .withMenuType(EMenuType.COMBINED)
                 .withItemTitle(EMenuItemPosition.LEFT, "a1", true)
-                .withItem(EMenuItemPosition.LEFT, "a2", VaadinIcons.HOME, HomePage.class, true)
-                .withItem(EMenuItemPosition.LEFT, "a3", VaadinIcons.COG, DefaultPage.class, true)
+                .withCustomComponent(new Label(), EAlignment.TOP) // adding a custom Component the MenuType decides what to do with it
+                .withCustomComponent(new CustomMenuItem(new Label("test1234"), EMenuPosition.LEFT, EAlignment.BOTTOM))
+                .withItem("a2", VaadinIcons.HOME, HomePage.class, true)
+                .withItem("a3", VaadinIcons.COG, DefaultPage.class, true)
                 .withItem(
-                        MenuItemBuilder.get("a4", VaadinIcons.FILE_TREE, HomePage.class, true)
-                                .withSubMenuItem("a5", VaadinIcons.HOME, HomePage.class, true)
-                                .withSubMenuItem("a6", VaadinIcons.HOME, HomePage.class, true)
-                                .withSubMenuItem("a7", VaadinIcons.HOME, HomePage.class, true)
+                        MenuItemBuilder.get("a4", VaadinIcons.FILE_TREE, HomePage.class, true) // parent Elements
+                                .withSubMenuItem("a5", VaadinIcons.HOME, HomePage.class, true) // children
+                                .withSubMenuItem("a6", VaadinIcons.HOME, HomePage.class, true) // ...
+                                .withSubMenuItem("a7", VaadinIcons.HOME, HomePage.class, true) // ...
                                 .withSubMenuItem("a8", VaadinIcons.HOME, HomePage.class, true)
                                 .withStyleName(ETopMenuPosition.RIGHT.toString())
                                 .build()
                 ).withItem(
-                        MenuItemBuilder.get("a9", VaadinIcons.FILE_TREE, HomePage.class, true)
+                        MenuItemBuilder.get("a9", VaadinIcons.FILE_TREE, HomePage.class, true) // parent Elements
                                 .withSubMenuItem(
-                                        MenuItemBuilder.get("a10", VaadinIcons.HOME, HomePage.class, true)
-                                                .withSubMenuItem("a11", VaadinIcons.HOME, HomePage.class, true)
-                                                .withSubMenuItem("a12", VaadinIcons.HOME, HomePage.class, true)
+                                        MenuItemBuilder.get("a10", VaadinIcons.HOME, HomePage.class, true) // child and parent
+                                                .withSubMenuItem("a11", VaadinIcons.HOME, HomePage.class, true) // sub sub child
+                                                .withSubMenuItem("a12", VaadinIcons.HOME, HomePage.class, true) // ...
                                                 .build()
-                                ).withSubMenuItem("a13", VaadinIcons.HOME, HomePage.class, true)
-                                .withSubMenuItem("a14", VaadinIcons.HOME, HomePage.class, true)
+                                ).withSubMenuItem("a13", VaadinIcons.HOME, HomePage.class, true) // sub child
+                                .withSubMenuItem("a14", VaadinIcons.HOME, HomePage.class, true) // ...
                                 .withSubMenuItem("a15", VaadinIcons.HOME, HomePage.class, true)
                                 .withStyleName(ETopMenuPosition.RIGHT.toString())
                                 .build()
-                ).withItem(null, VaadinIcons.SIGN_OUT.getHtml() + "Logout", MemberPage.class, false)
+                ).withItem("Logout", VaadinIcons.SIGN_OUT, MemberPage.class, false)
                 .build();
         setContent(hybridMenu);
         VaadinSession.getCurrent().setAttribute(HybridMenu.class, hybridMenu);
